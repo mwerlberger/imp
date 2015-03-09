05cf5e7ec3d0db41673e00fe7d6279b4f8ceb1fb
Manuel Werlberger <code@werlberger.org>
2015-03-09 22:11:02 +0100

    added simple plain huber-l1 model for stereo estimation; untested

--- modules/cudepth/src/cu_k_stereo_ctf_warping_level_huber_l1.cuh
+++ modules/cudepth/src/cu_k_stereo_ctf_warping_level_huber_l1.cuh
@@ -11,25 +11,6 @@
 namespace imp {
 namespace cu {
 
-
-//-----------------------------------------------------------------------------
-template<typename Pixel>
-__global__ void k_preconditioner(Pixel* xi, size_type stride,
-                                 std::uint32_t width, std::uint32_t height,
-                                 // std::uint32_t roi_x, std::uint32_t roi_y,
-                                 float lambda, Texture2D ix_tex)
-{
-  const int x = blockIdx.x*blockDim.x + threadIdx.x;
-  const int y = blockIdx.y*blockDim.y + threadIdx.y;
-
-  if (x<width && y<height)
-  {
-    Pixel ix;
-    ix_tex.fetch(ix, x, y);
-    xi[y*stride+x] = 4 + lambda*lambda * ix*ix;
-  }
-}
-
 //-----------------------------------------------------------------------------
 /** restricts the udpate to +/- lin_step around the given value in lin_tex
  * @note \a d_srcdst and the return value is identical.
@@ -57,24 +38,43 @@ __global__ void k_primalUpdate(PPixel* d_u, PPixel* d_u_prev, const size_type st
                                const float lambda, const float tau,
                                const float lin_step,
                                Texture2D u_tex, Texture2D u0_tex,
-                               Texture2D pu_tex, Texture2D q_tex,
-                               Texture2D ix_tex, Texture2D xi_tex)
+                               Texture2D pu_tex, Texture2D ix_tex)
 {
   const int x = blockIdx.x*blockDim.x + threadIdx.x;
   const int y = blockIdx.y*blockDim.y + threadIdx.y;
 
   if (x<width && y<height)
   {
+    float u0 = u0_tex.fetch<float>(x,y);
     float u_prev = u_tex.fetch<float>(x, y);
-    float q = q_tex.fetch<float>(x, y);
+    float it = it_tex.fetch<float>(x, y);
     float ix = ix_tex.fetch<float>(x, y);
-    float xi = xi_tex.fetch<float>(x, y);
 
     float div = dpAd(pu_tex, x, y, width, height);
 
-    float u = u_prev - tau/xi * (-div + lambda*ix*q);
+    float u = u_prev + tau * div;
 
+    // prox operator
+    float prox = it + (u-u0)*ix;
+    prox /= max(1e-5f, ix*ix);
+    float tau_lambda = tau*lambda;
+
+    if(prox < -tau_lambda)
+    {
+      u += tau_lambda*ix;
+    }
+    else if(prox > tau_lambda)
+    {
+      u -= tau_lambda*ix;
+    }
+    else
+    {
+      u -= prox*ix;
+    }
+
+    // restrict update step because of linearization only valid in small neighborhood
     u = k_linearized_update(u, u0_tex, lin_step, x, y);
+
     d_u[y*stride+x] = u;
     d_u_prev[y*stride+x] = 2.f*u - u_prev;
   }
@@ -83,36 +83,21 @@ __global__ void k_primalUpdate(PPixel* d_u, PPixel* d_u_prev, const size_type st
 //-----------------------------------------------------------------------------
 template<typename PPixel, typename DPixel>
 __global__ void k_dualUpdate(DPixel* d_pu, const size_type stride_pu,
-                             PPixel* d_q, const size_type stride_q,
                              std::uint32_t width, std::uint32_t height,
-                             const float lambda, const float eps_u,
-                             const float sigma, const float eta,
-                             Texture2D u_prev_tex, Texture2D u0_tex,
-                             Texture2D pu_tex, Texture2D q_tex,
-                             Texture2D ix_tex, Texture2D it_tex)
+                             const float eps_u, const float sigma,
+                             Texture2D u_prev_tex, Texture2D pu_tex)
 {
   const int x = blockIdx.x*blockDim.x + threadIdx.x;
   const int y = blockIdx.y*blockDim.y + threadIdx.y;
   if (x<width && y<height)
   {
-    const float sigma_by_eta = sigma/eta;
-
     // update pu
     float2 du = dp(u_prev_tex, x, y);
     float2 pu = pu_tex.fetch<float2>(x,y);
-    pu  = (pu + sigma_by_eta*du) / (1.f + sigma_by_eta*eps_u);
+    pu  = (pu + sigma*du) / (1.f + sigma*eps_u);
     pu = pu / max(1.0f, length(pu));
 
     d_pu[y*stride_pu+x] = {pu.x, pu.y};
-
-    // update q
-    float u_prev = u_prev_tex.fetch<float>(x, y);
-    float u0 = u0_tex.fetch<float>(x, y);
-    float q = q_tex.fetch<float>(x, y);
-    float ix = ix_tex.fetch<float>(x, y);
-    float it = it_tex.fetch<float>(x, y);
-    q += lambda*sigma_by_eta * (it + ix*(u_prev-u0));
-    d_q[y*stride_q+x] = max(-1.f, min(1.f, q));
   }
 }
 
