65aaf2b9533808e50c20eef99c7c2dab4d8fb674
Manuel Werlberger <code@werlberger.org>
2015-03-18 13:58:41 +0100

    possibility to change lambda in the RofNode with rqt_reconfigure

--- ros/ros_denoising/src/rof.cpp
+++ ros/ros_denoising/src/rof.cpp
@@ -1,18 +1,23 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 
+#include <dynamic_reconfigure/server.h>
+#include <imp_ros_denoising/RofConfig.h>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
 #include <imp/core/image_cv.hpp>
 #include <imp/cuimgproc/cu_rof_denoising.cuh>
 #include <imp/io/opencv_bridge.hpp>
+#include <imp/cucore/cu_utils.hpp>
 
 #include <sensor_msgs/Image.h>
 
 
 namespace imp {
 
+//------------------------------------------------------------------------------
 class RofNode
 {
 public:
@@ -20,6 +25,9 @@ public:
   ~RofNode() = default;
 
   void imgCb(const sensor_msgs::ImageConstPtr& img_msg);
+
+  void paramCb(imp_ros_denoising::RofConfig& config, uint32_t level);
+
 
 private:
   imp::cu::RofDenoising8uC1::Ptr rof_;
@@ -29,6 +37,7 @@ private:
   imp::cu::ImageGpu8uC1::Ptr denoised_;
 };
 
+//------------------------------------------------------------------------------
 void RofNode::imgCb(const sensor_msgs::ImageConstPtr &img_msg)
 {
   cv::Mat mat;
@@ -43,12 +52,12 @@ void RofNode::imgCb(const sensor_msgs::ImageConstPtr &img_msg)
   }
 
   imp::Size2u im_size((std::uint32_t)mat.cols, (std::uint32_t)mat.rows);
-  ROS_INFO("im_size: %dx%d", im_size.width(), im_size.height());
 
   if (!rof_ || !cv_img_ || !img_ || im_size != cv_img_->size())
   {
     rof_.reset(new imp::cu::RofDenoising8uC1());
-    //rof_->init(im_size);
+//    rof_->init(im_size);
+
     cv_img_.reset(new imp::ImageCv8uC1(im_size));
     cv_denoised_.reset(new imp::ImageCv8uC1(im_size));
     img_.reset(new imp::cu::ImageGpu8uC1(im_size));
@@ -61,55 +70,40 @@ void RofNode::imgCb(const sensor_msgs::ImageConstPtr &img_msg)
   denoised_->copyTo(*cv_denoised_);
   cv::imshow("input", cv_img_->cvMat());
   cv::imshow("denoised", cv_denoised_->cvMat());
-  cv::waitKey(30);
+  cv::waitKey(1);
 }
 
-}
-
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+//------------------------------------------------------------------------------
+void RofNode::paramCb(imp_ros_denoising::RofConfig& config, uint32_t level)
 {
-  ROS_INFO("hmmmmmmmm");
-  try
-  {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "mono8")->image);
-    cv::waitKey(30);
-  }
-  catch (std::exception& e)
-  {
-    ROS_ERROR("Could not process existing input image. Exception: %s", e.what());
-  }
+  if(!rof_)
+    return;
+  rof_->params().lambda = imp::cu::max(1e-6, config.lambda);
 }
 
+
+}
+
+//==============================================================================
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imp_rof_denoising");
+
+
   ros::NodeHandle nh;
   ROS_INFO("testing the RofNode");
-
   imp::RofNode rof_node;
+
+  // reconfigure stuff
+  dynamic_reconfigure::Server<imp_ros_denoising::RofConfig> server;
+  dynamic_reconfigure::Server<imp_ros_denoising::RofConfig>::CallbackType f;
+  f = boost::bind(&imp::RofNode::paramCb, &rof_node, _1, _2);
+  server.setCallback(f);
+
+
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub = it.subscribe("bluefox_ros_node/image_raw", 1, &imp::RofNode::imgCb, &rof_node);
-//  image_transport::Subscriber sub = it.subscribe("bluefox_ros_node/image_raw", 1, &imageCallback);
+
   ros::spin();
   return EXIT_SUCCESS;
 }
-
-
-//int main(int argc, char **argv)
-//{
-//  ros::init(argc, argv, "IMP ROF Denoising");
-//  ros::NodeHandle nh;
-//  {
-//    imp::cu::RofDenoising8uC1 rof;
-
-//    // subscribe to cam msgs
-////    std::string cam_topic(vk::getParam<std::string>("imp/rof/cam_topic", "camera/image_raw"));
-////    image_transport::ImageTransport it(nh);
-//    //image_transport::Subscriber it_sub = it.subscribe(cam_topic, 5, &svo::VoNode::imgCb, &vo_node);
-
-//  }
-
-//  std::cout << "hmmmm" << std::endl;
-//  ros::spin();
-//  return EXIT_SUCCESS;
-//}
