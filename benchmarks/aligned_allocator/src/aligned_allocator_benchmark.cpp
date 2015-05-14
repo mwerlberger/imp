7db80feb34a6a6e1fd56716fc821ad7c7a25ee9a
Manuel Werlberger <code@werlberger.org>
2015-05-14 16:17:53 +0200

    benchmarking aligned allocations

--- benchmarks/aligned_allocator/src/aligned_allocator_benchmark.cpp
+++ benchmarks/aligned_allocator/src/aligned_allocator_benchmark.cpp
@@ -13,14 +13,31 @@ int main(int argc, char* argv[])
 
   const size_t memory_size = 1e6;
   int memaddr_align = 32;
+  std::uint64_t num_rounds = 1e8;
 
-  imp::SingleShotTimer timer_posix_memalign("posix_memalign");
-
-  for (std::uint64_t i=0; i<1e6; ++i)
   {
-    std::uint8_t* p_data_aligned;
-    posix_memalign((void**)&p_data_aligned, memaddr_align, memory_size);
+    imp::SingleShotTimer timer("posix_memalign");
 
+    for (std::uint64_t i=0; i<num_rounds; ++i)
+    {
+      std::uint8_t* p_data_aligned;
+      posix_memalign((void**)&p_data_aligned, memaddr_align, memory_size);
+      free(p_data_aligned);
+    }
+
+    LOG(INFO) << "posix_memalign: " << std::fixed << (double)timer.elapsedMs().count()/num_rounds << " ms / alloc+free";
+  }
+
+  {
+    imp::SingleShotTimer timer("aligned_alloc");
+
+    for (std::uint64_t i=0; i<num_rounds; ++i)
+    {
+      std::uint8_t* p_data_aligned = (std::uint8_t*)aligned_alloc(memaddr_align, memory_size);
+      free(p_data_aligned);
+    }
+
+    LOG(INFO) << "aligned_alloc: " << std::fixed << (double)timer.elapsedMs().count()/num_rounds << " ms / alloc+free";
   }
 
 }
