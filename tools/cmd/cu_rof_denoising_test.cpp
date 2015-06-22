bba96e296e5fd4ebe475579f2b3198410dc8e6bd
Manuel Werlberger <code@werlberger.org>
2015-06-22 10:21:45 +0200

    more output and energy computation for both, 8u and 32f

--- tools/cmd/cu_rof_denoising_test.cpp
+++ tools/cmd/cu_rof_denoising_test.cpp
@@ -33,13 +33,19 @@ int main(int argc, char** argv)
             new imp::cu::ImageGpu8uC1(*cu_im));
 
       imp::cu::RofDenoising8uC1 rof;
-      std::cout << "\n" << rof << std::endl << std::endl;
+      rof.params().primal_dual_energy_check_iter = 10;
+      rof.params().primal_dual_gap_tolerance = 1e-3;
+
+      std::cout << "\n" << rof << std::endl;
       rof.denoise(cu_im_denoised, cu_im);
 
       // show results
       imp::cu::cvBridgeShow("input 8u", *cu_im);
       imp::cu::cvBridgeShow("denoised 8u", *cu_im_denoised);
     }
+
+    std::cout << "-------------------------------------------------------------"
+              << std::endl << std::endl;
 
     // ROF denoising 32fC1
     {
@@ -51,6 +57,8 @@ int main(int argc, char** argv)
       imp::cu::RofDenoising32fC1 rof;
       rof.params().primal_dual_energy_check_iter = 10;
       rof.params().primal_dual_gap_tolerance = 1e-3;
+
+      std::cout << "\n" << rof << std::endl;
       rof.denoise(cu_im_denoised, cu_im);
 
       imp::cu::cvBridgeShow("input 32f", *cu_im);
