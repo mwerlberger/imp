47c77352bdbc94f420cdf799d97626013eb223a5
Manuel Werlberger <code@werlberger.org>
2015-06-23 13:22:00 +0200

    fixed fcn params in header

--- imp_cu_core/include/imp/cu_core/cu_math.cuh
+++ imp_cu_core/include/imp/cu_core/cu_math.cuh
@@ -15,7 +15,7 @@ template<typename Pixel, imp::PixelType pixel_type>
 void minMax(const ImageGpu<Pixel, pixel_type>& img, Pixel& min, Pixel& max);
 
 template<typename Pixel>
-void minMax(const Texture2D& img_tex, double& sum, const imp::Roi2u& roi);
+void minMax(const Texture2D& img_tex, Pixel& min, Pixel& max, const imp::Roi2u& roi);
 
 //template<typename Pixel, imp::PixelType pixel_type>
 //void sum(const ImageGpu<Pixel, pixel_type>& img, double& sum);
