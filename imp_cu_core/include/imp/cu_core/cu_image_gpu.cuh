9272f114ca23d644492e5937bd26b166f45e587c
Manuel Werlberger <code@werlberger.org>
2015-07-15 11:19:33 +0200

    removed commented include

--- imp_cu_core/include/imp/cu_core/cu_image_gpu.cuh
+++ imp_cu_core/include/imp/cu_core/cu_image_gpu.cuh
@@ -9,8 +9,6 @@
 #include <imp/cu_core/cu_exception.hpp>
 #include <imp/cu_core/cu_memory_storage.cuh>
 #include <imp/core/image.hpp>
-//#include <imp/cu_core/cu_gpu_data.cuh>
-
 #include <imp/cu_core/cu_pixel_conversion.hpp>
 
 
